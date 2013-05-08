3d4f465534e4716895c3c426ecb7a468efafa16e
enginkizil <engin_kizil@hotmail.com>
2013-05-08 22:48:46 +0300

    Update FeedEk.js

--- js/FeedEk.js
+++ js/FeedEk.js
@@ -1,7 +1,46 @@
-/*
-* FeedEk jQuery RSS/ATOM Feed Plugin v1.1.2
-* http://jquery-plugins.net/FeedEk/FeedEk.html
-* Author : Engin KIZIL http://www.enginkizil.com
-* http://opensource.org/licenses/mit-license.php  
+/* FeedEk jQuery RSS/ATOM Feed Plugin v1.1.2
+*  http://jquery-plugins.net/FeedEk/FeedEk.html
+*  Author : Engin KIZIL http://www.enginkizil.com
+*  http://opensource.org/licenses/mit-license.php  
 */
-(function (e) { e.fn.FeedEk = function (t) { var n = { FeedUrl: "http://rss.cnn.com/rss/edition.rss", MaxCount: 5, ShowDesc: true, ShowPubDate: true, CharacterLimit: 0, TitleLinkTarget: "_blank" }; if (t) { e.extend(n, t) } var r = e(this).attr("id"); var i; e("#" + r).empty().append('<div style="padding:3px;"><img src="loader.gif" /></div>'); e.ajax({ url: "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + n.MaxCount + "&output=json&q=" + encodeURIComponent(n.FeedUrl) + "&hl=en&callback=?", dataType: "json", success: function (t) { e("#" + r).empty(); var s = ""; e.each(t.responseData.feed.entries, function (e, t) { s += '<li><div class="itemTitle"><a href="' + t.link + '" target="' + n.TitleLinkTarget + '" >' + t.title + "</a></div>"; if (n.ShowPubDate) { i = new Date(t.publishedDate); s += '<div class="itemDate">' + i.toLocaleDateString() + "</div>" } if (n.ShowDesc) { if (n.DescCharacterLimit > 0 && t.content.length > n.DescCharacterLimit) { s += '<div class="itemContent">' + t.content.substr(0, n.DescCharacterLimit) + "...</div>" } else { s += '<div class="itemContent">' + t.content + "</div>" } } }); e("#" + r).append('<ul class="feedEkList">' + s + "</ul>") } }) } })(jQuery)
+
+(function ($) {
+    $.fn.FeedEk = function (opt) {
+        var def = $.extend({
+            FeedUrl: "http://rss.cnn.com/rss/edition.rss",
+            MaxCount: 5,
+            ShowDesc: true,
+            ShowPubDate: true,
+            CharacterLimit: 0,
+            TitleLinkTarget: "_blank"
+        }, opt);
+
+        var id = $(this).attr("id");
+        var i;
+        $("#" + id).empty().append('<img src="loader.gif" />');
+        $.ajax({
+            url: "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + def.MaxCount + "&output=json&q=" + encodeURIComponent(def.FeedUrl) + "&hl=en&callback=?",
+            dataType: "json",
+            success: function (data) {
+                $("#" + id).empty();
+                var s = "";
+                $.each(data.responseData.feed.entries, function (e, item) {
+                    s += '<li><div class="itemTitle"><a href="' + item.link + '" target="' + def.TitleLinkTarget + '" >' + item.title + "</a></div>";
+                    if (def.ShowPubDate) {
+                        i = new Date(item.publishedDate);
+                        s += '<div class="itemDate">' + i.toLocaleDateString() + "</div>";
+                    }
+                    if (def.ShowDesc) {
+                        if (def.DescCharacterLimit > 0 && item.content.length > def.DescCharacterLimit) {
+                            s += '<div class="itemContent">' + item.content.substr(0, def.DescCharacterLimit) + "...</div>";
+                        }
+                        else {
+                            s += '<div class="itemContent">' + item.content + "</div>";
+                        }
+                    }
+                });
+                $("#" + id).append('<ul class="feedEkList">' + s + "</ul>");
+            }
+        });
+    };
+})(jQuery);
