2b031b65f13fa06b17dc5e8d7c28532e93033894
Gabe Ormsby <gormsby@umn.edu>
2014-10-28 12:56:48 -0500

    UMN Libraries customizations.

--- js/FeedEk.js
+++ js/FeedEk.js
@@ -2,6 +2,11 @@
 * FeedEk jQuery RSS/ATOM Feed Plugin v2.0
 * http://jquery-plugins.net/FeedEk/FeedEk.html  https://github.com/enginkizil/FeedEk
 * Author : Engin KIZIL http://www.enginkizil.com   
+* Modifications by gormsby@umn.edui to address the following needs:
+* - Requesting feeds over https to avoid mixed content warnings/blocks.
+* - Breaking summary on word boundary after
+*   http://jquery-plugins.net/FeedEk/FeedEk.html#comment-1324823216.
+* - Changing format of html output to correspond to our home page needs.
 */
 
 (function ($) {
@@ -18,15 +23,15 @@
         }, opt);
 
         var id = $(this).attr("id"), i, s = "",dt;
-        $("#" + id).empty().append('<img src="loader.gif" />');
+        $("#" + id).empty().append('<i class="icon-spin"></i>');
 
         $.ajax({
-            url: "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + def.MaxCount + "&output=json&q=" + encodeURIComponent(def.FeedUrl) + "&hl=en&callback=?",
+            url: "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + def.MaxCount + "&output=json&q=" + encodeURIComponent(def.FeedUrl) + "&hl=en&callback=?",
             dataType: "json",
             success: function (data) {
                 $("#" + id).empty();
                 $.each(data.responseData.feed.entries, function (e, item) {
-                    s += '<li><div class="itemTitle"><a href="' + item.link + '" target="' + def.TitleLinkTarget + '" >' + item.title + "</a></div>";
+                    s += '<p><strong><a href="' + item.link + '" target="' + def.TitleLinkTarget + '" >' + item.title + "</a></strong><br />";
                     
                     if (def.ShowPubDate){
                         dt= new Date(item.publishedDate);
@@ -43,14 +48,15 @@
                     }
                     if (def.ShowDesc) {
                         if (def.DescCharacterLimit > 0 && item.content.length > def.DescCharacterLimit) {
-                            s += '<div class="itemContent">' + item.content.substr(0, def.DescCharacterLimit) + "...</div>";
+                            s += '<div class="itemContent">' + item.content.substr(0, def.DescCharacterLimit).split(" ").slice(0,-1).join(" ") + "...</div>";
                         }
                         else {
                             s += '<div class="itemContent">' + item.content + "</div>";
                         }
                     }
+                    s+= '</p>';
                 });
-                $("#" + id).append('<ul class="feedEkList">' + s + "</ul>");
+                $("#" + id).append(s);
             }
         });
     };
