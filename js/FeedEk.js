82f727f7e65d2098a053447126cef3c475c46d1a
enginkizil <engin_kizil@hotmail.com>
2019-01-20 21:09:44 +0300

    Update FeedEk.js

--- js/FeedEk.js
+++ js/FeedEk.js
@@ -1,67 +1,62 @@
 /*
-* FeedEk jQuery RSS/ATOM Feed Plugin v3.0 with YQL API
+* FeedEk jQuery RSS/ATOM Feed Plugin v3.1 
 * http://jquery-plugins.net/FeedEk/FeedEk.html  https://github.com/enginkizil/FeedEk
 * Author : Engin KIZIL http://www.enginkizil.com   
 */
-
 (function ($) {
-    $.fn.FeedEk = function (opt) {
-        var def = $.extend({
-            MaxCount: 5,
-            ShowDesc: true,
-            ShowPubDate: true,
-            DescCharacterLimit: 0,
-            TitleLinkTarget: "_blank",
-            DateFormat: "",
-            DateFormatLang:"en"
-        }, opt);
-        
-        var id = $(this).attr("id"), i, s = "", dt;
-        $("#" + id).empty();
-        if (def.FeedUrl == undefined) return;       
-        $("#" + id).append('<img src="loader.gif" />');
+	$.fn.FeedEk = function (opt) {
+		var def = $.extend({
+			MaxCount: 5,
+			ShowDesc: true,
+			ShowPubDate: true,
+			DescCharacterLimit: 0,
+			TitleLinkTarget: "_blank",
+			DateFormat: "",
+			DateFormatLang: "en"
+		}, opt);
 
-        var YQLstr = 'SELECT channel.item FROM feednormalizer WHERE output="rss_2.0" AND url ="' + def.FeedUrl + '" LIMIT ' + def.MaxCount;
+		var id = $(this).attr("id"), i, s = "", dt;
+		$("#" + id).empty();
+		if (def.FeedUrl == undefined) return;
+		$("#" + id).append('<img src="loader.gif" />');
+		$.ajax({
+			url: "https://feed.jquery-plugins.net/load?url=" + encodeURIComponent(def.FeedUrl) + "&maxCount=" + def.MaxCount,
+			dataType: "json",
+			success: function (result) {
+				$("#" + id).empty();
+				if (result.data == null)
+					return;
+				$.each(result.data, function (e, itm) {
+					s += '<li><div class="itemTitle"><a href="' + itm.link + '" target="' + def.TitleLinkTarget + '" >' + itm.title + '</a></div>';
+					if (def.ShowPubDate) {
+						dt = new Date(itm.publishDate);
+						s += '<div class="itemDate">';
+						if ($.trim(def.DateFormat).length > 0) {
+							try {
+								moment.lang(def.DateFormatLang);
+								s += moment(dt).format(def.DateFormat);
+							}
+							catch (e) { s += dt.toLocaleDateString(); }
+						}
+						else {
+							s += dt.toLocaleDateString();
+						}
+						s += '</div>';
+					}
+					if (def.ShowDesc) {
+						s += '<div class="itemContent">';
+						if (def.DescCharacterLimit > 0 && itm.description.length > def.DescCharacterLimit) {
+							s += itm.description.substring(0, def.DescCharacterLimit) + '...';
+						}
+						else {
+							s += itm.description;
+						}
+						s += '</div>';
+					}
+				});
 
-        $.ajax({
-            url: "https://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent(YQLstr) + "&format=json&diagnostics=false&callback=?",
-            dataType: "json",
-            success: function (data) {
-                $("#" + id).empty();
-                if (!(data.query.results.rss instanceof Array)) {
-                    data.query.results.rss = [data.query.results.rss];
-                }
-                $.each(data.query.results.rss, function (e, itm) {
-                    s += '<li><div class="itemTitle"><a href="' + itm.channel.item.link + '" target="' + def.TitleLinkTarget + '" >' + itm.channel.item.title + '</a></div>';
-                    
-                    if (def.ShowPubDate){
-                        dt = new Date(itm.channel.item.pubDate);
-                        s += '<div class="itemDate">';
-                        if ($.trim(def.DateFormat).length > 0) {
-                            try {
-                                moment.lang(def.DateFormatLang);
-                                s += moment(dt).format(def.DateFormat);
-                            }
-                            catch (e){s += dt.toLocaleDateString();}                            
-                        }
-                        else {
-                            s += dt.toLocaleDateString();
-                        }
-                        s += '</div>';
-                    }
-                    if (def.ShowDesc) {
-                        s += '<div class="itemContent">';
-                         if (def.DescCharacterLimit > 0 && itm.channel.item.description.length > def.DescCharacterLimit) {
-                            s += itm.channel.item.description.substring(0, def.DescCharacterLimit) + '...';
-                        }
-                        else {
-                            s += itm.channel.item.description;
-                         }
-                         s += '</div>';
-                    }
-                });
-                $("#" + id).append('<ul class="feedEkList">' + s + '</ul>');
-            }
-        });
-    };
+				$("#" + id).append('<ul class="feedEkList">' + s + '</ul>');
+			}
+		});
+	};
 })(jQuery);
